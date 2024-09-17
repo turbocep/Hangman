11886cd7730c93e82b01866ae7a555f71e940422
turbocep <162988238+turbocep@users.noreply.github.com>
2024-09-17 16:17:27 +0200

    add basic gameplay loop.
    Added basic ability to play game. It's missing
    lots, like checking for wins and validating
    input.

--- lib/game.rb
+++ lib/game.rb
@@ -7,10 +7,7 @@
 file_path = 'guess_words.txt'
 GUESS_WORD = File.readlines(file_path).map(&:chomp).sample
 
-tried_letters = []
-
-
-#Should probably turn into method to call more easily.
+#Obstruct guess word for player based on their guessed letters.
 def obstruct_guess_word(guess_word, tried_letters)
   GUESS_WORD.split("").map do |letter|
     if tried_letters.include?(letter)
@@ -21,11 +18,26 @@ def obstruct_guess_word(guess_word, tried_letters)
   end.join(" ")
 end
 
-puts GUESS_WORD
-puts obstruct_guess_word(GUESS_WORD, tried_letters)
-#I could use classes and then display the guess word based on methods (like SSN)
+letters_tried = []
+wrong_guesses_left = 7
+round = 0
+#Represents what happened the previous move. If 1, you had a correct guess. 0 if wrong. I know this is bad practice. I don't care right now. Creative process.
+last_move = 0
 
-#Let's just make the basic thing first. 
+loop do
+  #Show appropriate hangman graphic.
+  #Just write uncompact garbage first (like with writing) and then improve later.
+  if round == 0
+    puts "New game started!"
+  end
+  puts "Letters tried: #{letters_tried.join(" ")}"
+  puts "Wrong guesses left: #{wrong_guesses_left}"
+  puts 'ERROR. Insufficient budget for hangman graphics support. Get a job!'
+  puts "Guess word: #{obstruct_guess_word(GUESS_WORD, letters_tried)}"
+  puts "Enter guess letter or 'quit' to quit game:"
+  guess = gets.chomp
+  letters_tried.push(guess)
+end
 
 
 
