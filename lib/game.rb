ccbfc3b9fecaef2e7c54fd26882fddd5a6ff867e
turbocep <162988238+turbocep@users.noreply.github.com>
2024-09-17 15:58:53 +0200

    turn guess word obstructor into method and test.

--- lib/game.rb
+++ lib/game.rb
@@ -10,15 +10,19 @@ GUESS_WORD = File.readlines(file_path).map(&:chomp).sample
 tried_letters = []
 
 
-guess_word_visual = GUESS_WORD.split("").map do |letter|
-  if tried_letters.include?(letter)
-    letter
-  else
-    "_"
-  end
-end.join(" ")
+#Should probably turn into method to call more easily.
+def obstruct_guess_word(guess_word, tried_letters)
+  GUESS_WORD.split("").map do |letter|
+    if tried_letters.include?(letter)
+      letter
+    else
+      "_"
+    end
+  end.join(" ")
+end
 
-p guess_word_visual
+puts GUESS_WORD
+puts obstruct_guess_word(GUESS_WORD, tried_letters)
 #I could use classes and then display the guess word based on methods (like SSN)
 
 #Let's just make the basic thing first. 
