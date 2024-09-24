afbcb7d511eb25ab8ac0664e6d24886462a55aa0
turbocep <162988238+turbocep@users.noreply.github.com>
2024-09-24 12:59:49 +0200

    rework menu loop and make game quittable.

--- lib/game.rb
+++ lib/game.rb
@@ -27,81 +27,69 @@ save_path = './saves.json'
 game_saves = JSON.parse(File.read(save_path))
 
 
-
-
-
-#Welcome Screen
 puts "Welcome to Hangman!"
+#Menu Loop
 loop do
-  puts "<< Enter 'n' to start new game or 'l' to load a saved game:"
-  user_input = gets.downcase.chomp
-  break if ['n', 'l'].include?(user_input)
-  puts "Invalid input. Try again."
-end
+  #Main Menu
+  loop do
+    puts "--Main Menu--"
+    puts "<< Enter 'n' to start new game or 'l' to load a saved game:"
+    user_input = gets.downcase.chomp
+    break if ['n', 'l'].include?(user_input)
+    puts "Invalid input. Try again."
+  end
 
-if user_input == 'n'
-  puts 'Creating new game'
-  begin
-    puts 'Combobulating guess word.'
-    guess_word = get_random_word()
-  rescue
-    puts 'Something went wrong. Trying again.'
-    retry
-  end
-  puts 'Guess word combobulated!'
-elsif user_input == 'l'
-  puts "Saved Games:"
-  #Load saved games.
-  puts 'Index   Save name'
-  game_saves.each_with_index do |game, index|
-    puts "  #{index + 1}  => #{game["name"]}"
-  end
-  puts "#{game_saves.length}/10 saves made"
-  puts '>> Select a save by entering its index:'
-  loop do
-    user_input = gets.chomp.to_i
-    break if user_input > 0 && user_input <= game_saves.length
-    puts "Invalid input. Try again."
-  end
-  #Save Selected
-  selected_file = game_saves[user_input - 1]
-  puts '>> Enter l to load or d to delete this save'
-  loop do
-    user_input = gets.chomp.downcase
-    break if ['d', 'l'].include?(user_input)
-    puts "Invalid input. Try again."
-  end
-  if user_input == 'd'
-    #Delete selected save
-    game_saves.delete(selected_file)
-    #Write saves file with save array excluding deleted save
-    File.write(save_path, JSON.dump(game_saves))
+  if user_input == 'n'
+    puts 'Creating new game'
+    begin
+      puts 'Combobulating guess word.'
+      guess_word = get_random_word()
+    rescue
+      puts 'Something went wrong. Trying again.'
+      retry
+    end
+    puts 'Guess word combobulated!'
+    break
   elsif user_input == 'l'
-    #Load the new game now.
-    #Need a way to read each variable into the game properly.
-    #Assign each loaded game value to each variable here.
-    round = selected_file["round"]
-    letters_tried = selected_file["letters_checked"]
-    wrong_guesses_left = selected_file["wrong_guesses_left"]
-    guess_word = selected_file["guess_word"]
+    puts "Saved Games:"
+    #Load saved games.
+    puts 'Index   Save name'
+    game_saves.each_with_index do |game, index|
+      puts "  #{index + 1}  => #{game["name"]}"
+    end
+    puts "#{game_saves.length}/10 saves made"
+    puts '>> Select a save by entering its index:'
+    loop do
+      user_input = gets.chomp.to_i
+      break if user_input > 0 && user_input <= game_saves.length
+      puts "Invalid input. Try again."
+    end
+    #Save Selected
+    selected_file = game_saves[user_input - 1]
+    puts '>> Enter l to load or d to delete this save'
+    loop do
+      user_input = gets.chomp.downcase
+      break if ['d', 'l'].include?(user_input)
+      puts "Invalid input. Try again."
+    end
+    if user_input == 'd'
+      #Delete selected save
+      game_saves.delete(selected_file)
+      #Write saves file with save array excluding deleted save
+      File.write(save_path, JSON.dump(game_saves))
+    elsif user_input == 'l'
+      #Loads save game variables
+      round = selected_file["round"]
+      letters_tried = selected_file["letters_tried"]
+      wrong_guesses_left = selected_file["wrong_guesses_left"]
+      guess_word = selected_file["guess_word"]
+      puts "Resuming game '#{selected_file["name"]}'"
+      break
+    end
+  else
+    puts "Something went wrong."
   end
-  #This shouldn't just start a new game. Later problem.
-else
-  puts "Something went wrong."
 end
-
-
-
-
-
-
-
-#Paths
-letters_tried = []
-wrong_guesses_left = 7
-round = 0
-#Represents what happened the previous move. If 1, you had a correct guess. 0 if wrong. I know this is bad practice. I don't care right now. Creative process.
-last_move = 0
 
 loop do
   #Show appropriate hangman graphic.
@@ -110,19 +98,35 @@ loop do
     puts "New game started!"
   end
   puts guess_word
-  puts "Letters tried: #{letters_tried.join(" ")}"
+  letters_tried_formatted = letters_tried.nil? ? "" : letters_tried.join(" ")
+  puts "Letters tried: #{letters_tried_formatted}"
   puts "Wrong guesses left: #{wrong_guesses_left}"
-  puts 'ERROR. Insufficient budget for hangman graphics support. Get a job!'
+  puts "\n\n\n\n\n\n\n\n\n\n\n\n"
   obstructed_word = obstruct_guess_word(guess_word, letters_tried)
   puts "Guess word: #{obstructed_word}"
   if obstructed_word.split(" ").join == guess_word
     puts "Game over. You won!"
     break
   end
-  puts "Enter guess letter or 'quit' to quit game:"
-  guess = gets.chomp
-  letters_tried.push(guess)
-  
+  loop do
+    puts "Enter guess letter or 'quit' to quit game:"
+    guess = gets.chomp.downcase
+    if guess == "quit"
+      puts "Quitting game"
+      exit = true
+      break
+    elsif letters_tried.include?(guess)
+      puts "You've already tried this letter. Try a different one."
+    elsif !%w[
+      a b c d e f g h i j k l m n o p q r s t u v w x y z
+      ].include?(guess)
+      puts "Invalid input"
+    else
+      letters_tried.push(guess)
+      break
+    end
+  end
+  break if exit
 end
 
 
